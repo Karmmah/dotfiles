f34517381dc56b8f8453dfeea854bbe629a25574
patrick <kramer.patrick@protonmail.com>
2025-01-12 15:26:57 +0100

    added bashrc; updated setup

---
+++ .bashrc
@@ -0,0 +1,5 @@
+# set up bash prompt style
+#PS1="\[\e]133;D;$?\e\\\e]133;A\e\\\]\[$(ppwd)\]\u@\h:\w>"
+#PS1="\[\e]133;D;$?\e\\\e]133;A\e\\\]\[$(ppwd)\]\u@\h:\w> \[\e]133;B\e\\\]"
+PS1="\h-\u-$PWD\n> "
+#PS1="\h-\u-$PWD \! \#\n> "
--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
-echo "doesn't work yet"
-#pwd
+#!/bin/sh
 
-#ln -s pwd/.vimrc /home/pk/.vimrc
-#echo copied vimrc
+echo Setting up bashrc
+printf "\n# get custom bashrc settings\nsource $HOME/dotfiles/.bashrc" >> $HOME/.bashrc
 
+echo Setting up tmux.conf
+ln -s .tmux.conf $HOME/
+
+echo Setting up vimrc
+ln -s .vimrc $HOME/
